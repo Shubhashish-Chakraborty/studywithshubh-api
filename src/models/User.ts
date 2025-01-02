fbe0d47d79056d9ee5988a261670f8707981ecda
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-02 22:54:08 +0530

    added a default value to the courses

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    courses: { type: String }
+    courses: { type: String , default:"" }
     // courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
 
