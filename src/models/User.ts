1b97c5dedbe820ee0db9943b3b9b408476d56ece
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-02 22:51:39 +0530

    user model little bit changed

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+    courses: { type: String }
+    // courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
 
 export const UserModel = mongoose.model('users', UserSchema);
