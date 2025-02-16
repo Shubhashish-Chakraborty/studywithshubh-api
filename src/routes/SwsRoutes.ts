72bcababc7317b88fe9bba3a9d412e11c9ec4337
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-16 10:25:57 +0530

    vulnerability fixed - authentication related

--- src/routes/SwsRoutes.ts
+++ src/routes/SwsRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { signup , login, adminLogin } from '../controllers/authControllers';
 import { addCourse, getCourse, updateCourse } from '../controllers/courseController';
+import { authenticate } from '../middlewares/auth';
 
 export const swsRouter = Router();
 
 swsRouter.route('/signup').post(signup);
 swsRouter.route('/login').post(login);
 swsRouter.route('/login/admin').post(adminLogin);
-swsRouter.route('/courses').post(addCourse);
-swsRouter.route('/courses/update').patch(updateCourse);
-swsRouter.route('/courses/:courseName').get(getCourse);
+swsRouter.route('/courses').post(authenticate , addCourse);
+swsRouter.route('/courses/update').patch(authenticate , updateCourse);
+swsRouter.route('/courses/:courseName').get(authenticate , getCourse);
